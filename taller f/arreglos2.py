d958e3612c3849a055c2e93dca8d565ba0d55742
amadorsin <acacaok@gmail.com>
2025-04-23 11:16:27 -0400

    holahola

---
+++ taller f/arreglos2.py
@@ -0,0 +1,28 @@
+playlistdelcurso = [
+    "for the evening.mp3",
+    "Assumptions.mp3",
+    "Alice.mp3",
+    "Dark thougts.mp3",
+    "rather lie.mp3",
+]
+
+print(playlistdelcurso)
+print("\n")
+print(playlistdelcurso[2])
+
+print("tamaño del arreglo")
+listsize = len(playlistdelcurso)
+print(listsize)
+print("\n la ultima cancion es: \n")
+print(playlistdelcurso[listsize - 1])
+
+print("otro ejemplo")
+palabra = "amador"
+print(len(palabra))
+#con corchetes para indicar la cantidad
+
+print("\nImprimir todas las letras de una palabra")
+npalabra = "girafarig"
+for i in range(len(npalabra)):
+    print(npalabra[i])
+
